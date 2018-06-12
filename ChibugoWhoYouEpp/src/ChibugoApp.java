acc04ac327ca04d7d8af57bb1f5fa0de39a0d727
Joseph Marcus <josephmarcus@Josephs-MacBook-Pro.local>
2018-06-12 19:47:56 +0100

    Initial commit

---
+++ ChibugoWhoYouEpp/src/ChibugoApp.java
@@ -0,0 +1,14 @@
+/**
+ * Created by josephmarcus on 12/06/2018.
+ */
+public class ChibugoApp {
+    public static void main(String[] args){
+        printFavouriteColours();
+    }
+
+    public static void printFavouriteColours(){
+        System.out.println("Yellow");
+    }
+
+
+}
